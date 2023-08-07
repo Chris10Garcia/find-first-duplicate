f18cb07fff0fabdf4359a821c7274cb944dcb09f
Christian Garcia <bones192nd@gmail.com>
2023-08-07 11:15:15 -0400

    minor misunderstanding first go at problem. redid it

--- python/find_first.py
+++ python/find_first.py
@@ -1,6 +1,29 @@
 
 
 """
+Was unclear the first time submitting
+not 2 values next to eachother, but just a duplicate value first found
+"""
+
+"""
+0, 1, 5, 4, 3, 2, 5, 4, 3, 3
+
+answer = 5
+
+
+lets use a set, 
+as you go through each element in the array, insert the element into the set
+if the value exists, return the value,
+else, add the element into the set
+if nothing else is returned, return -1
+
+"""
+
+
+
+
+# previous attempt
+"""
 an array of numbers
 Find the first duplicate (two repeating values adjacent to eachother)
 if no duplicates, return -1
@@ -41,33 +64,48 @@ element_i == element_(i +1)
 """
 
 
+# def main(array):
+    
+#     if len(array) < 2:
+#         return -1
+    
+#     for i in range(len(array) -1):
+#         if array[i] == array[i +1]:
+#             return array[i]
+    
+#     return -1
+
+
 def main(array):
-    
-    if len(array) < 2:
-        return -1
-    
-    for i in range(len(array) -1):
-        if array[i] == array[i +1]:
-            return array[i]
-    
+
+    array_set = set()
+
+    for element in array:
+        if element in array_set:
+            return element
+        else:
+            array_set.add(element)
+        
     return -1
+
+
 
 
 
 if __name__=="__main__":
 
     test_values = [
-        [5, 4, 3, 10, 3, 8, 8, 0, 5, 6],
+        [5, 4, 3, 10, 3, 8, 0, 5, 6],
         [1, 2, 3, 4],
-        [3, 4, 4, 200, 4, 3],
+        [3, 4, 200, 4, 3],
         [],
         [5],
         [50000, 100, 300, 20],
-        [10, 10]
+        [10, 0, 5, 10, 5]
     ]
 
     test_answers = [
-        8,
+        3,
         -1,
         4,
         -1,
